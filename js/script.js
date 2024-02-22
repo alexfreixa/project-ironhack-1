f39fa27c198749066da5f361ce80300df86eb8ac
alexfreixa <alexfreixa18@gmail.com>
2024-02-22 19:28:00 +0100

    API get information through Promise (JS), navbar links fixed, small CSS additions

--- js/script.js
+++ js/script.js
@@ -15,3 +15,40 @@ document.addEventListener("DOMContentLoaded", () => {
 
 });
 
+
+
+window.addEventListener("load", (event) => {
+    displayProjects(3);
+});
+
+
+async function displayProjects(limit = 0) {
+    try {
+        const response = await fetch("https://raw.githubusercontent.com/ironhack-jc/mid-term-api/main/projects");
+        const jsonResponse = await response.json();
+
+        const publicProjects = jsonResponse.slice(0, limit);
+
+        const container = document.getElementById('cards-container');
+
+        var buildHtml = '';
+
+        publicProjects.forEach((project) => {
+
+        buildHtml += `<article class="span4"><div class="card">
+                            <div class="project-img">
+                                <img class="max-w-100" src="${project.image}">
+                            </div>
+                            <div class="card-text">
+                            <h3 class="intro medium">${project.name}</h3>
+                            <p>${project.description}</p>
+                            <a href="/project-ironhack-1/project.html" class="link medium">Learn more</a>
+                            </div>
+                        </div></article>`;
+        });
+        container.innerHTML = buildHtml;
+    } catch (error) {
+        // Handle error or a rejected Promise
+        console.log("Something went wrong!", error);
+    }
+}
---
+++ js/single-project.js
@@ -0,0 +1,31 @@
+window.addEventListener("load", (event) => {
+    displaySingleProject("1");
+  });
+
+
+  async function displaySingleProject(uuid) {
+  try {
+      const response = await fetch("https://raw.githubusercontent.com/ironhack-jc/mid-term-api/main/projects");
+      const jsonResponse = await response.json();
+
+      const singleProject = jsonResponse.find(project => project.uuid === uuid);
+
+      const title = document.getElementById('ptitle');
+      const description = document.getElementById('pdescription');
+      const date = document.getElementById('pdate');
+      const image = document.getElementById('pimage');
+      const content = document.getElementById('pcontent');
+      const link = document.getElementById('plink')
+
+      
+        title.innerHTML = singleProject.name; // Use singleProject instead of project
+        description.innerHTML = singleProject.description;
+        date.innerHTML = singleProject.completed_on;
+        image.src = singleProject.image; // Use src for an image element
+        content.innerHTML = singleProject.content;
+
+  } catch (error) {
+      // Handle error or a rejected Promise
+      console.log("Something went wrong!", error);
+  }
+}
